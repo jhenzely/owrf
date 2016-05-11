bc787dc3b5e4f428bc8f4beee851daeee8875995
Jaroslav Henzely <jaroslav.henzely@gmail.com>
2016-05-11 12:09:14 +0200

    Update sta.sh

--- sta.sh
+++ sta.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 sleep 7
-
 uci set wireless.@wifi-iface[0].disabled=0
-uci commit wireless
-sleep 10
+sleep 7
 
 while true ; do
 	if [ $(iwinfo | grep -c "ESSID: unknown") -ge 1 ]; then
 	uci set wireless.@wifi-iface[0].disabled=1; wifi; exit 0
 	fi
-	sleep 1
+	sleep 3
 done
