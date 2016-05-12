a5a712841dd91f822ec86e6934c936409420d25d
Jaroslav Henzely <jaroslav.henzely@gmail.com>
2016-05-12 14:15:55 +0200

    Update work_in_progress.sh

--- work_in_progress.sh
+++ work_in_progress.sh
@@ -1,26 +1,18 @@
 ## Untested
 
 #!/bin/sh
-
 stas=$(uci show wireless | grep "sta" | grep -o \\[[0-9]\])
 
 kill_stas ()
 {
-i=0; for i in $stas ; do
+  i=0
+  for i in $stas ; do
   uci set wireless.@wifi-iface[$i].disabled=1
 done
 }
 
-sleep 7 
-
-uci set wireless.@wifi-iface[0].disabled=0
-uci commit wireless
-
-sleep 7
-
 while true ; do
-	if [ $(iwinfo | grep -c "ESSID: unknown") -ge 1 ]; then
-	kill_stas; wifi; exit
-	fi
-	sleep 3
+  if [ $(iwinfo | grep -c "ESSID: unknown") -ge 1 ]; then
+  kill_stas; wifi; exit; fi
+  sleep 3
 done
