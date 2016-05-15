291d403c003a60953033b40d7159e6bf190fc27c
Jaroslav Henzely <jaroslav.henzely@gmail.com>
2016-05-15 23:17:54 +0200

    Update owrf.sh

--- owrf.sh
+++ owrf.sh
@@ -15,3 +15,7 @@ echo "Adding script to start after boot"
 
 chmod 755 /etc/config/sta.sh && echo "sta.sh O.K" || echo "Adding sta.sh FAILED" ; exit 1
 chmod 755 /etc/rc.local && printf "\n%s\n%s\n%s\n%s\n" "/etc/rc.local O.K" "Well done !" "Everything was successfully set-up, and owrf should work now !" "Jaroslav Henzely jaroslav.henzely@gmail.com" || echo "/etc/rc.local FAILED, script is not able to start on boot" ; exit 1
+
+stas=$(uci show wireless | grep "sta" | grep -o \[0-9])
+uci set wireless.@wifi-iface[$stas].disabled=0
+uci commit wireless
