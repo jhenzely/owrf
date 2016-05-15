195aa728bc53c869d68e21d01c801524f1195539
Jaroslav Henzely <jaroslav.henzely@gmail.com>
2016-05-15 21:26:11 +0200

    Update sta.sh

--- sta.sh
+++ sta.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 sleep 7
-uci set wireless.@wifi-iface[0].disabled=0
-sleep 7
+stas=""
+stas=$(uci show wireless | grep "sta" | grep -o \[0-9])
+uci set wireless.@wifi-iface[$stas].disabled=0; wifi
+stas 5
+
+kill_stas ()
+{
+ for i in $stas ; do
+   uci set wireless.@wifi-iface[$i].disabled=1
+   wifi
+ done
+}
 
 while true ; do
-	if [ $(iwinfo | grep -c "ESSID: unknown") -ge 1 ]; then
-	uci set wireless.@wifi-iface[0].disabled=1; wifi; exit 0
-	fi
-	sleep 3
+ if [ $(iwinfo | grep -c "ESSID: unknown") -ge 1 ]; then
+   kill_stas ; wifi; exit 0
+   fi
+   sleep 3 
 done
